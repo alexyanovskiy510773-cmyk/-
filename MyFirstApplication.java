ea39f46ff236f02622f255af22ba224bedbf0ccb
alexyanovskiy510773-cmyk <alexyanovskiy510773@gmail.com>
2026-06-07 20:47:52 +0300

    Delete MyFirstApplication.java

--- MyFirstApplication.java
+++
@@ -1,9 +0,0 @@
-
-/**
- * Simple Java application to verify setup.
- */
-class MyFirstApplication {
-    public static void main(String[] args) {
-        System.out.println("I've coded, compiled, and run my first Java program!");
-    }
-}
