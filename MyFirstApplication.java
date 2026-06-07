a2d3bdd4bb166359f9d567074d42ba4c13d39c36
alexyanovskiy510773-cmyk <alexyanovskiy510773@gmail.com>
2026-06-07 20:46:15 +0300

    MyFirstApl

---
+++ MyFirstApplication.java
@@ -0,0 +1,9 @@
+
+/**
+ * Simple Java application to verify setup.
+ */
+class MyFirstApplication {
+    public static void main(String[] args) {
+        System.out.println("I've coded, compiled, and run my first Java program!");
+    }
+}
